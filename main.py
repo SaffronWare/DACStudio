bf22648ae361a49d188f61540911cc3f8137890c
SaffronWare <aryanshadzi@gmail.com>
2026-08-05 05:59:05 -0400

    somethings coming up loweky, am i the archuitect

--- main.py
+++ main.py
@@ -1,4 +1,61 @@
 import dearpygui.dearpygui as dpg
+from abc import ABC, abstractmethod
+
+NODE_TYPES = {
+    "OUTPUT": 1,
+    "INPUT": 0,
+    "INTER": 0.5
+}
+
+
+class Field(ABC):
+    def __init__(self):
+        self.label = None
+        self.value = None
+
+    @abstractmethod
+    def imGuiInputDisplayer(self):
+        pass
+
+    @abstractmethod
+    def imGuiOutputDisplayer(self):
+        pass
+
+
+class GenericFloatField(Field):
+    def __init__(self, label, default_or_start):
+        self.value = default_or_start
+        self.label = label
+
+    def imGuiInputDisplayer(self):
+        dpg.add_input_float(label=self.label, width=150)
+
+
+class Node:
+    def __init__(self):
+        self.label = ""
+        self.type = None
+        self.input_fields = []
+        self.output_fields = []
+
+    def display(self):
+        with dpg.node(label=self.label):
+            for input_field in self.input_fields:
+                with dpg.node_attribute(label=input_field.label):
+                    input_field.imGuiInputDisplayer()
+            for output_field in self.output_fields:
+                with dpg.node_attribute(label=output_field.label):
+                    output_field.imGuiOutputDisplayer()
+
+class AccDataNode(Node):
+    def __init__(self):
+        self.label = "Accelerometer Data"
+        self.type = NODE_TYPES["INPUT"]
+        self.input_fields = []
+        self.output_fields = []
+
+
+NODES = []
 
 
 dpg.create_context()
@@ -21,27 +78,11 @@ def delink_callback(sender, app_data):
 with dpg.window(label="Tutorial", width=400, height=400):
 
     with dpg.node_editor(callback=link_callback, delink_callback=delink_callback):
-        with dpg.node(label="Node 1"):
-            with dpg.node_attribute(label="Node A1"):
-                dpg.add_input_float(label="F1", width=150)
+        for node in NODES:
+            node.display()
 
-            with dpg.node_attribute(label="Node A2", attribute_type=dpg.mvNode_Attr_Output):
-                dpg.add_input_float(label="F2", width=150)
+dpg.create_viewport(title='DAC Studio', width=600, height=300)
 
-        with dpg.node(label="Node 2"):
-            with dpg.node_attribute(label="Node A3"):
-                dpg.add_input_float(label="F3", width=200)
-
-            with dpg.node_attribute(label="Node A4", attribute_type=dpg.mvNode_Attr_Output):
-                dpg.add_input_float(label="F4", width=200)
-
-dpg.create_viewport(title='Custom Title', width=600, height=300)
-
-with dpg.window(label="Example Window"):
-    dpg.add_text("Hello, world")
-    dpg.add_button(label="Save")
-    dpg.add_input_text(label="string", default_value="Quick brown fox")
-    dpg.add_slider_float(label="float", default_value=0.273, max_value=1)
 
 dpg.setup_dearpygui()
 dpg.show_viewport()
