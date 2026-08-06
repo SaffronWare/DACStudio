09a2e0582d9f8160c8503c50e30293e23a531cb9
SaffronWare <aryanshadzi@gmail.com>
2026-08-06 09:44:08 -0400

    i MIGHT be cooking this time, well see

--- mainV2.py
+++ mainV2.py
@@ -5,15 +5,22 @@ OUTPUT_FIELD = 1
 INPUT_FIELD = 0
 
 class NodeField:
-    def __init__(self, label, type, parent, range=None):
+    def __init__(self, label, type, parent, vrange=None):
         self.draw_id = None
         self.drawn = False
-        self.connection = None
-        self.type = INPUT_FIELD # OUT/IN:PUT_FIELD
-
+        self.connection = None # not updated if its an output field
+        self.label = label
+        self.range = vrange
+        self.parent = parent
 
     def update_draw(self):
-        
+        if not self.drawn:
+            if self.connection is None:
+                dpg.add_text(self.label)
+            elif self.range is None:
+                dpg.add_float_value(label=self.parent.label + self.label)
 
 class Node(ABC):
-    
+    def __init__(self):
+        self.label = None 
+        self.nodes = {INPUT_FIELD: [], OUTPUT_FIELD: []}
