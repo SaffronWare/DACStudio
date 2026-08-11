d36fbc9ec9d039e131b0818e88b9f33268f09075
SaffronWare <aryanshadzi@gmail.com>
2026-08-11 17:23:10 -0400

    im dying but were close

--- main..py
+++ main..py
@@ -92,7 +92,7 @@ class Node(ABC):
         i = 0
         out = ""
         while i < len(srcstr):
-            print(out)
+            #print(out)
             char = srcstr[i]
             if char  == "~":
                 i += 1
@@ -335,10 +335,100 @@ def configuration_menu():
 
 
 def create_program():
-    with open("program.ino", "w"):
+    with open("program.ino", "w") as f:
         # somehow have to choose order 🥹💔
+        f.write(
+"""#include "FastIMU.h"
+#include <Wire.h>
+#include <EEPROM.h>
+
+// This relies on the FastIMU library caught  from https://github.com/LiquidCGS/FastIMU/blob/main/examples/Calibrated_sensor_output/Calibrated_sensor_output.ino
+
+MPU6500 IMU;
+AccelData accelData;
+GyroData gyroData;
+calData calib = { 0 };
+
+void setup()
+{
+  Wire.begin();
+  Wire.setClock(400000);
+  Serial.begin(115200);
+  while (!Serial)
+  {
+    ;
+  }
+
+  int err = IMU.init(calib, 0x68);
+  if (err != 0) {
+    Serial.print("Error initializing IMU: ");
+    Serial.println(err);
+    while (true) {
+      ;
+    }
+  }
+
+  if (err != 0) {
+    Serial.print("Error Setting range: ");
+    Serial.println(err);
+    while (true) {
+      ;
+    }
+  }
+}
+
+int i = 0;
+void loop()
+{
+  if (i == 0)
+  {
+    delay(1000 * 120);
+  }
+"""
+)
+        f.write(
+"""if ((i+1) * sizeof(float) < 256)
+  {
+    IMU.update(); 
+    IMU.getAccel(&accelData);
+    Serial.println("wow\n");
+  
+    EEPROM.put((i) * sizeof(float), accelData.accelX);
+
+    Serial.println(accelData.accelX);
+    Serial.println("wow\n");
+  }
+  delay(50);
+  i+=1;
+}""")
+        nodes = list(Node.registered_nodes.values())
+        # want to sort in a way where each node only depends on those like before it
+        for i in range(len(nodes)):
+            node = nodes[i]
+            for j in range(len(nodes)):
+                if j != i:
+                    node_other = nodes[j]
+                    if j < i:
+                        for field in node_other.nodes[INPUT_FIELD]:
+                            if field.connection is not None:
+                                if field.connection.parent.label == node.label:
+                                    nodes.pop(j)
+                                    nodes.insert(i, node_other)
+                                    print("field is", end=" ")
+                                    print(field.connection.parent.label)
+                    else:
+                        for field in node.nodes[OUTPUT_FIELD]:
+                            if field.connection is not None:
+                                if field.connection.parent.label == node_other.label:
+                                    nodes.pop(j)
+                                    nodes.insert(i-1, node_other)
+                                    print("field is", end=" ")
+                                    print(field.connection.parent.label)
+                
+                
         for lab,node in Node.registered_nodes.items():
-            print(node) 
+            f.write(str(node))
+            pass
     return
 
 
---
+++ program.ino
@@ -0,0 +1,69 @@
+#include "FastIMU.h"
+#include <Wire.h>
+#include <EEPROM.h>
+
+// This relies on the FastIMU library caught  from https://github.com/LiquidCGS/FastIMU/blob/main/examples/Calibrated_sensor_output/Calibrated_sensor_output.ino
+
+MPU6500 IMU;
+AccelData accelData;
+GyroData gyroData;
+calData calib = { 0 };
+
+void setup()
+{
+  Wire.begin();
+  Wire.setClock(400000);
+  Serial.begin(115200);
+  while (!Serial)
+  {
+    ;
+  }
+
+  int err = IMU.init(calib, 0x68);
+  if (err != 0) {
+    Serial.print("Error initializing IMU: ");
+    Serial.println(err);
+    while (true) {
+      ;
+    }
+  }
+
+  if (err != 0) {
+    Serial.print("Error Setting range: ");
+    Serial.println(err);
+    while (true) {
+      ;
+    }
+  }
+}
+
+int i = 0;
+void loop()
+{
+  if (i == 0)
+  {
+    delay(1000 * 120);
+  }
+if ((i+1) * sizeof(float) < 256)
+  {
+    IMU.update(); 
+    IMU.getAccel(&accelData);
+    Serial.println("wow
+");
+  
+    EEPROM.put((i) * sizeof(float), accelData.accelX);
+
+    Serial.println(accelData.accelX);
+    Serial.println("wow
+");
+  }
+  delay(50);
+  i+=1;
+}
+    s_ServoYaw = Gyroscope0_GyroscopeX;
+    s_ServoRoll = Gyroscope0_GyroscopeZ;
+    s_ServoPitch = Gyroscope0_GyroscopeY;
+
+    Gyroscope0_GyroscopeX = s_GyroX;
+    Gyroscope0_GyroscopeY = s_GyroY;
+    Gyroscope0_GyroscopeZ = s_GyroZ;
